9fc7b97a930e1185b3fe2b4fc0490d3683697314
sano <sanopl@gmail.com>
2025-03-02 18:43:44 +0100

    new platform version;

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
-import org.jetbrains.grammarkit.tasks.GenerateLexerTask
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 fun properties(key: String) = providers.gradleProperty(key)
 fun environment(key: String) = providers.environmentVariable(key)
 
 plugins {
     id("java") // Java support
-    alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
+    alias(libs.plugins.intelliJPlatform) // Gradle IntelliJ Plugin
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
     alias(libs.plugins.qodana) // Gradle Qodana Plugin
     alias(libs.plugins.kover) // Gradle Kover Plugin
@@ -29,6 +29,11 @@ kotlin {
 // Configure project's dependencies
 repositories {
     mavenCentral()
+
+    // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 idea {
@@ -50,26 +55,82 @@ java {
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    testImplementation(libs.junit)
+
+    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+    intellijPlatform {
+        create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
+
+        // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
+        bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
+
+        // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
+        plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
+
+        pluginVerifier()
+        zipSigner()
+        testFramework(TestFrameworkType.Platform)
+    }
 }
 
 
 // Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    val platformType = properties("platformType").get()
-    pluginName = properties("pluginName")
-    version = properties("platformVersion")
-    type = platformType
-    updateSinceUntilBuild.set(true)
-    downloadSources = false
-    val platformPlugins = when (platformType) {
-        "IU" -> properties("ideaPlugins")
-        "IC" -> properties("ideaCommunityPlugins")
-        "CL" -> properties("clionPlugins")
-        else -> throw IllegalArgumentException("Unknown IDE type: $type, supported types: IU, IC, CL")
+intellijPlatform {
+
+    pluginConfiguration {
+        version = providers.gradleProperty("pluginVersion")
+
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+            val start = "<!-- Plugin description -->"
+            val end = "<!-- Plugin description end -->"
+
+            with(it.lines()) {
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+            }
+        }
+
+        val changelog = project.changelog // local variable for configuration cache compatibility
+        // Get the latest available change notes from the changelog file
+        changeNotes = providers.gradleProperty("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        }
+
+        ideaVersion {
+            sinceBuild = providers.gradleProperty("pluginSinceBuild")
+            untilBuild = providers.gradleProperty("pluginUntilBuild")
+        }
     }
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins = platformPlugins.map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        channels = providers.gradleProperty("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
+    }
+
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
@@ -107,13 +168,24 @@ tasks {
         purgeOldFiles.set(true)
     }*/
 
-
-    val generateC3Lexer = task<GenerateLexerTask>("generateC3Lexer") {
-        sourceFile.set(file("src/main/java/org/c3lang/intellij/C3.flex"))
-        targetDir.set("src/main/gen/org/c3lang/intellij/lexer")
-        targetClass.set("C3Lexer")
-        purgeOldFiles.set(true)
-    }
+//    generateParser {
+//        dependsOn(compileJava, compileKotlin)
+//        sourceFile.set(file("src/main/java/org/c3lang/intellij/C3.bnf"))
+//        targetRoot.set("src/main/gen")
+//        pathToParser.set("/org/c3lang/intellij/parser/C3Parser.java")
+//        pathToPsiRoot.set("/org/c3lang/intellij/psi")
+//        purgeOldFiles.set(true)
+//        enabled = false
+//    }
+//
+//    generateLexer {
+//        dependsOn(compileJava, compileKotlin)
+//        sourceFile.set(file("src/main/java/org/c3lang/intellij/C3.flex"))
+//        targetDir.set("src/main/gen/org/c3lang/intellij/lexer")
+//        targetClass.set("C3Lexer")
+//        purgeOldFiles.set(true)
+//        enabled = false
+//    }
 
     patchPluginXml {
         version = properties("pluginVersion")
@@ -149,13 +221,13 @@ tasks {
 
     // Configure UI tests plugin
     // Read more: https://github.com/JetBrains/intellij-ui-test-robot
-    runIdeForUiTests {
-        systemProperty("robot-server.port", "8082")
-        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
-        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
-        systemProperty("jb.consents.confirmation.enabled", "false")
-        systemProperty("grammar.kit.gpub.max.level", 10000)
-    }
+//    runIdeForUiTests {
+//        systemProperty("robot-server.port", "8082")
+//        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
+//        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
+//        systemProperty("jb.consents.confirmation.enabled", "false")
+//        systemProperty("grammar.kit.gpub.max.level", 10000)
+//    }
 
     runIde {
         jvmArgs(
@@ -176,13 +248,6 @@ tasks {
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
         channels = properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) }
-    }
-
-    compileJava {
-        dependsOn(generateC3Lexer)
-    }
-    compileKotlin {
-        dependsOn(generateC3Lexer)
     }
 
 }
---
+++ src/test/kotlin/org/c3lang/intellij/C3Test.kt
@@ -0,0 +1,11 @@
+package org.c3lang.intellij
+
+import org.junit.Test
+
+class C3Test {
+
+    @Test
+    fun test1() {
+
+    }
+}
